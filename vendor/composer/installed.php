a8b3731e7d259ad6a4bfd57d397448f6e5f9422f
luukimhoang2k <luukimhoangdz2022@gmail.com>
2021-06-24 08:39:18 +0700

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8cba16f922e1088d250398c1947334e3c0bb67b9',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8cba16f922e1088d250398c1947334e3c0bb67b9',
+    ),
+  ),
+);
